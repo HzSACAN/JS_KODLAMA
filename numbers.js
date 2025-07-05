1259533fc74196ada8d85892720cac2c7b8d00b0
Hazret Saçan <asklanhazret51@gmail.com>
2025-07-05 18:56:43 +0300

    js dersleri

---
+++ numbers.js
@@ -0,0 +1,31 @@
+let sonuc;
+
+sonuc = 10;
+sonuc = "10";
+sonuc = Number("10");
+sonuc = parseInt("10.6");
+sonuc = parseFloat("10.6");
+sonuc = parseInt("10a");
+sonuc = parseInt("a10");
+
+sonuc = isNaN("10"); // not a number (boolean değer döndürür)
+
+let sayi = 47.1234567;
+
+sonuc = sayi.toPrecision(5);
+sonuc = sayi.toFixed(5);
+
+sonuc = Math.round(2.4);
+sonuc = Math.round(2.6);
+sonuc = Math.ceil(2.6);
+sonuc = Math.floor(2.6);
+sonuc = Math.sqrt(49);
+sonuc = Math.pow(2, 3);
+sonuc = Math.abs(-10);
+sonuc = Math.min(4, 66, 8, 3, 9);
+sonuc = Math.max(4, 66, 8, 3, 9);
+sonuc = Math.random();
+sonuc = Math.floor(Math.random() * 10);
+
+console.log(typeof sonuc);
+console.log(sonuc);
